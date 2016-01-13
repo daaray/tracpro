2462b51239a542235baf1e973075bd118aa287c0
Rebecca <rebecca@caktusgroup.com>
2016-01-13 17:24:17 -0500

    Factor out useful filters javascript

--- tracpro/static/js/poll-charts.js
+++ tracpro/static/js/poll-charts.js
@@ -102,43 +102,8 @@ jQuery.fn.extend({
 });
 
 $(function() {
-    /* Initialize date fields. */
-    $("input[class^='datepicker']").datepicker({
-        changeMonth: true,
-        changeYear: true,
-        selectOtherMonths: true,
-        showOtherMonths: true,
-        yearRange: "2013:" + new Date().getFullYear()
-    });
-
-    /* Update button text when filter form display is toggled. */
-    $('#filters').on('show.bs.collapse', function() {
-        $('#toggle-filters').text('Hide filters...');
-    });
-    $('#filters').on('hide.bs.collapse', function() {
-        $('#toggle-filters').text('Show filters...');
-    });
-
-    /* Don't submit custom date fields if they are not used. */
-    $('.filter-form #id_date_range').on('change', function() {
-        var showDates = $(this).val() === 'custom';
-        $('#filter-dates').toggleClass('hidden', !showDates);
-        $('#filter-dates').find('input').prop('disabled', !showDates)
-    }).change();
-
-    /* Don't submit empty form fields. */
-    $('.filter-form').on('submit', function() {
-        $(this).find('input,select').each(function(i, item) {
-          var self = $(this);
-          if (!self.val()) {
-            self.prop('disabled', true);
-          }
-        });
-        return true;
-    });
-
     /* Update numeric data display on the client side. */
-    $('#id_numeric').on('change', function() {
+    $('.filter-form #id_numeric').on('change', function() {
         $('.chart-numeric').chart_numeric();
     });
 
